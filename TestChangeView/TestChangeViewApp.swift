91d6749082b281d1387323408aa121c36b6f6dff
Tien Do <tiendq@gmail.com>
2021-08-10 19:37:24 +0700

    Initial Commit

---
+++ TestChangeView/TestChangeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestChangeViewApp.swift
+//  TestChangeView
+//
+//  Created by Tien Do on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TestChangeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
